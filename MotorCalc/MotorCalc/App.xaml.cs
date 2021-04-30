e4040251af11a130c476438b0b812aa36b5bc45c
Marcelo Pereira Santos <lemarcelo@outlook.com>
2021-04-29 23:49:15 -0300

    Add project files.

---
+++ MotorCalc/MotorCalc/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using MotorCalc.Services;
+using MotorCalc.Views;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MotorCalc
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+            DependencyService.Register<MockDataStore>();
+            MainPage = new AppShell();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ MotorCalc/MotorCalc/AppShell.xaml.cs
@@ -0,0 +1,23 @@
+﻿using MotorCalc.ViewModels;
+using MotorCalc.Views;
+using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace MotorCalc
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
+            Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+        }
+
+        private async void OnMenuItemClicked(object sender, EventArgs e)
+        {
+            await Shell.Current.GoToAsync("//LoginPage");
+        }
+    }
+}
