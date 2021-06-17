1b948fbc563f7fc9699592f513658c9e8758969b
Marcelo Pereira Santos <lemarcelo@outlook.com>
2021-06-17 19:39:12 -0300

    Alterações

---
+++ MotorCalc/MotorCalc/Models/Medidas.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MotorCalc.Models
+{
+    public class Medidas
+    {
+        private double _Pistao;
+
+        public double Pistao
+        {
+            get { return _Pistao; }
+            set { _Pistao = value; }
+        }
+
+        private double _Cilindro;
+
+        public double Cilindro
+        {
+            get { return _Cilindro; }
+            set { _Cilindro = value; }
+        }
+
+    }
+}
---
+++ MotorCalc/MotorCalc/Views/MedidasView.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using MotorCalc.Models;
+using Xamarin.Forms.Xaml;
+using System.Collections.ObjectModel;
+
+namespace MotorCalc.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MedidasView: ContentPage
+    {
+        public MedidasView()
+        {
+            InitializeComponent();
+            BindingContext = this;
+        }
+
+        public ObservableCollection<Medidas> ListMedidas = new ObservableCollection<Medidas>
+        {
+            new Medidas
+            {
+                Pistao = 1,
+                Cilindro = 2
+            }
+        };
+
+
+
+
+
+    }
+}
