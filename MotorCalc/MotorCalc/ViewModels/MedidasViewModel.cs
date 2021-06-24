52ab1b069e6ca5468f3d2081c74fe90270e58994
Marcelo Pereira Santos <lemarcelo@outlook.com>
2021-06-24 19:58:41 -0300

    Correções para a loja

--- MotorCalc/MotorCalc/ViewModels/MedidasViewModel.cs
+++ MotorCalc/MotorCalc/ViewModels/MedidasViewModel.cs
@@ -20,14 +20,31 @@ namespace MotorCalc.ViewModels
         {
             CollectionMedidas = new ObservableCollection<Medidas>();
             CollectionMedidas.Add(new Medidas() { DiametroP = "Diâmetro Pistão", Curso = "Curso Vb." });
-            CollectionMedidas.Add(new Medidas() { Descricao = "Titan 150", DiametroP = "57,3", Curso = "57,8" });
-            CollectionMedidas.Add(new Medidas() { Descricao = "Titan 150 Kit 190cc", DiametroP = "64,5", Curso = "57,8" });
-            CollectionMedidas.Add(new Medidas() { Descricao = "Titan 150 Kit Setentão 225cc", DiametroP = "70", Curso = "57,8" });
-            CollectionMedidas.Add(new Medidas() { Descricao = "XTZ/YBR 125", DiametroP = "54", Curso = "54" });
-            CollectionMedidas.Add(new Medidas() { Descricao = "Twister/Tornado", DiametroP = "73", Curso = "59.5" });
-            CollectionMedidas.Add(new Medidas() { Descricao = "XTZ 250 - Lander, Fazer...", DiametroP = "74", Curso = "58" });
-            CollectionMedidas.Add(new Medidas() { Descricao = "XR200", DiametroP = "63,5", Curso = "62,2" });
-            CollectionMedidas.Add(new Medidas() { Descricao = "Bros OHC", DiametroP = "63,5", Curso = "49,5" });
+            CollectionMedidas.Add(new Medidas() { Descricao = "Titan 150", DiametroP = "57,3 mm", Curso = "57,8 mm" });
+            CollectionMedidas.Add(new Medidas() { Descricao = "Titan 150 Kit 190cc", DiametroP = "64,5 mm", Curso = "57,8 mm" });
+            CollectionMedidas.Add(new Medidas() { Descricao = "Titan 150 Kit Setentão 225cc", DiametroP = "70 mm", Curso = "57, 8mm" });
+            CollectionMedidas.Add(new Medidas() { Descricao = "XTZ/YBR 125", DiametroP = "54 mm", Curso = "54 mm" });
+            CollectionMedidas.Add(new Medidas() { Descricao = "Twister/Tornado", DiametroP = "73 mm", Curso = "59. 5mm" });
+            CollectionMedidas.Add(new Medidas() { Descricao = "XTZ 250 - Lander, Fazer...", DiametroP = "74 mm", Curso = "58 mm" });
+            CollectionMedidas.Add(new Medidas() { Descricao = "XR200", DiametroP = "63,5 mm", Curso = "62,2 mm" });
+            CollectionMedidas.Add(new Medidas() { Descricao = "Bros OHC", DiametroP = "63,5 mm", Curso = "49,5 mm" });
+            CollectionMedidas.Add(new Medidas() { Descricao = "CRF 230", DiametroP = "65,5 mm", Curso = "66,2 mm" });
+            CollectionMedidas.Add(new Medidas() { Descricao = "XT 225", DiametroP = "70 mm", Curso = "58 mm" });
+            CollectionMedidas.Add(new Medidas() { Descricao = "Twister 293", DiametroP = "79 mm", Curso = "59,5 mm" });
+
+
+
+
+
+
+
+
+
+
+            //CollectionMedidas.Add(new Medidas() { Descricao = "", DiametroP = "mm", Curso = "mm" });
+
+
+
         }
     }
 }
