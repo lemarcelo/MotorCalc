c84e1d15e1e5a6ddc16e06ca83650d1432a60d30
Marcelo Pereira Santos <lemarcelo@outlook.com>
2021-06-25 11:11:01 -0300

    Mais dados para a base de peças

--- MotorCalc/MotorCalc/ViewModels/MedidasViewModel.cs
+++ MotorCalc/MotorCalc/ViewModels/MedidasViewModel.cs
@@ -25,11 +25,16 @@ namespace MotorCalc.ViewModels
             CollectionMedidas.Add(new Medidas() { Descricao = "XTZ/YBR 125", DiametroP = "54 mm", Curso = "54 mm" });
             CollectionMedidas.Add(new Medidas() { Descricao = "Twister/Tornado", DiametroP = "73 mm", Curso = "59. 5 mm" });
             CollectionMedidas.Add(new Medidas() { Descricao = "XTZ 250 - Lander, Fazer...", DiametroP = "74 mm", Curso = "58 mm" });
+            CollectionMedidas.Add(new Medidas() { Descricao = "CB Twister CRF 250f", DiametroP = "71 mm", Curso = "63,03 mm" });
             CollectionMedidas.Add(new Medidas() { Descricao = "XR200", DiametroP = "63,5 mm", Curso = "62,2 mm" });
             CollectionMedidas.Add(new Medidas() { Descricao = "Bros OHC", DiametroP = "63,5 mm", Curso = "49,5 mm" });
             CollectionMedidas.Add(new Medidas() { Descricao = "CRF 230", DiametroP = "65,5 mm", Curso = "66,2 mm" });
             CollectionMedidas.Add(new Medidas() { Descricao = "XT 225", DiametroP = "70 mm", Curso = "58 mm" });
-            CollectionMedidas.Add(new Medidas() { Descricao = "Twister 293", DiametroP = "79 mm", Curso = "59,5 mm" });
+            CollectionMedidas.Add(new Medidas() { Descricao = "Twister 291", DiametroP = "79 mm", Curso = "59,5 mm" });
+            CollectionMedidas.Add(new Medidas() { Descricao = "CB 300", DiametroP = "79 mm", Curso = "59,5 mm" });
+            CollectionMedidas.Add(new Medidas() { Descricao = "Falcon 400", DiametroP = "85 mm", Curso = "70 mm" });
+            CollectionMedidas.Add(new Medidas() { Descricao = "CBR 250r", DiametroP = "76 mm", Curso = "55 mm" });
+            CollectionMedidas.Add(new Medidas() { Descricao = "CG 160 Titan/Start", DiametroP = "57,3 mm", Curso = "63 mm" });
 
 
 
